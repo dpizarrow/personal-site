9e293797aa5bba241a1e5937e2c14cad17786ede
dpizarrow <diego.pizarro0@gmail.com>
2023-12-14 09:45:10 -0300

    chore: update meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import ModeToggle from "@/components/ModeToggle";
-
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">Hi! 👋</h1>
-      <p className="text-lg mt-4">
+    <div className="flex flex-col justify-center items-center h-screen px-4">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+        Hi! 👋
+      </h1>
+      <p className="text-md sm:text-lg mt-4 text-center">
         I&apos;m Diego, a Computer Science student at the University of Chile
       </p>
-      <p className="text-lg">
+      <p className="text-md sm:text-lg text-center">
         Check out my projects, learn more about me, and feel free to connect!
       </p>
     </div>
