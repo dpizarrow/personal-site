5107df7ac6c10d0befabe054c44f9cd82bb17c68
dpizarrow <diego.pizarro0@gmail.com>
2023-12-15 20:29:12 -0300

    add skillcard (temp commit)

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
+
+import SkillCard from "@/components/SkillCard";
+
+import Python from "@/assets/svg/python.svg";
+import Django from "@/assets/svg/django.svg";
+import FastAPI from "@/assets/svg/fastapi.svg";
+
 export default function Skills() {
+  const skills = [
+    {
+      name: "Python",
+      svg: Python,
+      subSkills: [
+        { name: "Django", svg: Django },
+        { name: "FastAPI", svg: FastAPI },
+      ],
+    },
+  ];
+
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-2xl">
-      Coming soon!
+    <div className="flex flex-col justify-center items-center px-4">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8">
+        Skills
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {skills.map((skill, index) => (
+          <SkillCard
+            key={index}
+            skillName={skill.name}
+            svgPath={skill.svg}
+            subSkills={skill.subSkills}
+          />
+        ))}
+      </div>
     </div>
   );
 }
