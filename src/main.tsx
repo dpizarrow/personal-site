fc7bad138dcc3b026bfa9f67701a477f921c0d96
dpizarrow <diego.pizarro0@gmail.com>
2024-03-15 11:26:23 -0300

    feat: add initial routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import About from "@/routes/about.tsx";
+import Contact from "@/routes/contact.tsx";
+import Skills from "@/routes/skills.tsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/skills", element: <Skills /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
